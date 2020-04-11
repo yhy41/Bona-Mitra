90b0fa9ae1fd8778459833f001db9a99715a4e57
ahmdsyfiq <ahmdsyfiq@gmail.com>
2020-04-12 05:44:19 +0700

    just input

--- application/views/pilihan.php
+++ application/views/pilihan.php
@@ -1,36 +1,47 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>pilihan masukan ke website</title>
-  <link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/style.css'); ?>"/>
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<!doctype html>
+<html lang="en">
+  <head>
+    <link type="text/css" rel="stylesheet" href="<?php echo base_url('assets/style.css'); ?>"/>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-</head>
-<body>
- <!--  buat tmapilan awal pada dengan ada tombol admin/apa lah dan dengan ada tombol guest -->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
+    <title>Klinik Bona</title>
+  </head>
+  <body>
+    <div class="pos-f-t">
+      <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+          <h5 class="text-white h4">Login User</h5>
+          <a class="nav-link" href="<?= site_url('pilihan/masukpasien') ?>">Login as Guest</a>
+          <a class="nav-link" href="<?= site_url('Login/index') ?>">Login as Administrator</a>
+        </div>
+      </div>
+      <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </nav>
+    </div>
+    <h1 style="padding: 150px; text-align: center;">Welcome to Klinik Bona Website!</h1>
+    <footer class="mastfoot mt-auto" style="text-align: center;">
+      <div class="inner">
+        <p>Tugas Besar - Pemrograman Web</p>
+      </div>
+    </footer>
 
-  <div class="table-responsive">          
-  <table class="table">
-    <thead>
-      <tr>
-        <td >
-          <form action="<?= site_url('Login/index') ?>" method="post">
-            <button  type="submit" class="btn btn-primary active" >Admin</button>
-          </form>
-        </td>
-        <td >
-          <!-- <form action=""> -->
-            <button type="submit" class="btn btn-primary active"><a href="<?= site_url('pilihan/masukpasien') ?>">Guest</button>
-          <!-- </form> -->
-        </td>
-      </tr>
-    </thead>
-  </table>
-  </div>
-</body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
 </html>
